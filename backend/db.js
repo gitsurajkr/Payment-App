257d12c029fea5fde97025f78f887cb88ba8b710
Suraj Kumar <kumarsuraj.islampur@gmail.com>
2024-08-21 23:03:51 +0530

    fixed database

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,11 @@
+require('dotenv').config(); 
+
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-});
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 const userSchema = new mongoose.Schema({
     username:{
         type : String,
